fb3ac2b93ae86531dcabc29547af1e8cdc528ec7
Steven Huang <orz0611@gmail.com>
2014-01-12 16:41:56 +0800

    Added user/post association

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140111145739) do
+ActiveRecord::Schema.define(version: 20140112083324) do
 
   create_table "posts", force: true do |t|
-    t.string "title"
-    t.string "url"
-    t.text   "description"
+    t.string  "title"
+    t.string  "url"
+    t.text    "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: true do |t|
