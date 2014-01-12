11cd91236e8adccd9fc17a4d83b3f39eb7e0f5e5
Steven Huang <orz0611@gmail.com>
2014-01-12 17:29:28 +0800

    comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140112083324) do
+ActiveRecord::Schema.define(version: 20140112085606) do
+
+  create_table "comments", force: true do |t|
+    t.text    "body"
+    t.integer "user_id"
+    t.integer "post_id"
+  end
 
   create_table "posts", force: true do |t|
     t.string  "title"
