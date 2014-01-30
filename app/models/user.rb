a8b81c2d1fcc643f8670ff5a12de0c0c8e08c6c7
Steven Huang <orz0611@gmail.com>
2014-01-30 10:44:40 +0800

    added register

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
+
+  has_secure_password validations: false
+
+  validate :username, presence: true, uniqueness: true
+  validate :password, presence: true, on: :create, length: {minium: 3}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
+
+  get 'register', to: 'users#new'
+  #get 'login', to: 'session#new'
+  #get 'logout', to: 'session#destroy' 
+
   resources :posts, except: :destroy do
     resources :comments, only: :create
   end
   resources :categories, only: [:create, :new, :show]
+  resources :users
 end
