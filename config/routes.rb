bfac0503e0ee18ceed80d73b9ab11bd831808695
Steven Huang <orz0611@gmail.com>
2014-02-02 16:27:46 +0800

    view/edit user

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,12 @@ PostitTemplate::Application.routes.draw do
       post :vote
     end
 
-   resources :comments, only: :create
+    resources :comments, only: :create do
+      member do
+        post :vote
+      end
+    end
   end
   resources :categories, only: [:create, :new, :show]
-  resources :users, only: :create
+  resources :users, only: [:create, :show, :edit]
 end
